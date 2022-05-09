7141bfe2499f55c9ed661683a1d7250c80158fc6
ktsoi <krystaltsoi@ucla.edu>
2022-05-09 15:49:14 -0700

    updated to 4 colors working, fixed refill buttons

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-var span2 = document.getElementsByClassName("close2")[0];
+// var span2 = document.getElementsByClassName("close2")[0];
 var span3 = document.getElementsByClassName("close3")[0];
 
 
@@ -16,9 +16,9 @@ document.getElementById("help").onclick = function() {
     document.getElementById("helpModal").style.display = "block";
 }
 
-document.getElementById("history").onclick = function() {
-    document.getElementById("histModal").style.display = "block";
-}
+// document.getElementById("history").onclick = function() {
+//     document.getElementById("histModal").style.display = "block";
+// }
 
 document.getElementById("submitButton").onclick = function() {
     document.getElementById("startModal").style.display = "block";
@@ -33,9 +33,9 @@ span.onclick = function() {
     document.getElementById("helpModal").style.display = "none";
 }
 
-span2.onclick = function() {
-    document.getElementById("histModal").style.display = "none"; //DOESNT WORK STILL
-}
+// span2.onclick = function() {
+//     document.getElementById("histModal").style.display = "none"; //DOESNT WORK STILL
+// }
 
 span3.onclick = function() {
     document.getElementById("startModal").style.display = "none"; //DOESNT WORK STILL
@@ -46,18 +46,18 @@ window.onclick = function(event) {
     if (event.target == document.getElementById("helpModal")) {
         document.getElementById("helpModal").style.display = "none";
     }
-    if (event.target == document.getElementById("histModal")) {
-        document.getElementById("histModal").style.display = "none";
-    }
+    // if (event.target == document.getElementById("histModal")) {
+    //     document.getElementById("histModal").style.display = "none";
+    // }
     if (event.target == document.getElementById("startModal")) {
         document.getElementById("startModal").style.display = "none";
     }
 }
 
 //closes modal when history color is clicked on
-function closeModalWhenHistColClicked(){
-    document.getElementById("histModal").style.display = "none";
-}
+// function closeModalWhenHistColClicked(){
+//     document.getElementById("histModal").style.display = "none";
+// }
 
 function closeModalWhenStartClicked(){
     document.getElementById("startModal").style.display = "none";
@@ -71,6 +71,28 @@ function changeColor(color){
     var element = document.getElementById("box");
     element.style.backgroundColor = newColor;	
 }
+function changeColor2(color){
+    var newColor = checkHash(color);
+    document.getElementsByName('hex2')[0].value= newColor;
+
+    var element = document.getElementById("box");
+    element.style.backgroundColor = newColor;	
+}
+function changeColor3(color){
+    var newColor = checkHash(color);
+    document.getElementsByName('hex3')[0].value= newColor;
+
+    var element = document.getElementById("box");
+    element.style.backgroundColor = newColor;	
+}
+function changeColor4(color){
+    var newColor = checkHash(color);
+    document.getElementsByName('hex4')[0].value= newColor;
+
+    var element = document.getElementById("box");
+    element.style.backgroundColor = newColor;	
+}
+
 function removeImage(){
     var element = document.getElementById("box");
     element.style.backgroundImage = 'none';	
@@ -90,11 +112,42 @@ function checkHash(val){
 }
  
 
-//for volume slider
+
+//for volume slider in progress
+// function volume(range, vol)
+
 var slider = document.getElementById("myRange");
 var output = document.getElementById("myVol");
+
 output.innerHTML = slider.value;
 
 slider.oninput = function() {
   output.innerHTML = this.value;
+}
+
+var slider2 = document.getElementById("myRange2");
+var output2 = document.getElementById("myVol2");
+
+output2.innerHTML = slider2.value;
+
+slider2.oninput = function() {
+  output2.innerHTML = this.value;
+}
+
+var slider3 = document.getElementById("myRange3");
+var output3 = document.getElementById("myVol3");
+
+output3.innerHTML = slider3.value;
+
+slider3.oninput = function() {
+  output3.innerHTML = this.value;
+}
+
+var slider4 = document.getElementById("myRange4");
+var output4 = document.getElementById("myVol4");
+
+output4.innerHTML = slider4.value;
+
+slider4.oninput = function() {
+  output4.innerHTML = this.value;
 }
